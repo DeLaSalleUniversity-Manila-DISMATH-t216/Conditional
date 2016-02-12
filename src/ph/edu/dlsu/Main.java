fee093f3324346e324cbd9c86cf700cb4c42770c
aenoncunanan <aenon_cunanan2@yahoo.com>
2016-02-12 13:47:21 +0800

    Initial commit

---
+++ src/ph/edu/dlsu/Main.java
@@ -0,0 +1,20 @@
+package ph.edu.dlsu;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int intValue = 0;
+
+        if (intValue > 0){
+            System.out.println("The value " +intValue+ " is greater then zero (0).");
+        }
+        else if (intValue < 0 ){
+            System.out.println("The value " +intValue+ " is less than zero (0).");
+        }
+        else{
+            System.out.println("The value is " +intValue+ ".");
+        }
+
+    }
+}
---
+++ src/ph/edu/dlsu/StringCompare.java
@@ -0,0 +1,14 @@
+package ph.edu.dlsu;
+
+public class StringCompare {
+
+    public static void main(String[] Args){
+
+        String stringValue = "Hello World!";
+
+        if (stringValue.equals("Hello World!")){
+            System.out.println("The value is: " +stringValue);
+        }
+    }
+
+}
